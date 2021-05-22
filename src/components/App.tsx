65236e78bb4e4a869f35d000fc0864cf85831f66
Stephen Tafoya <stephen@tafoya.dev>
2021-05-22 03:27:18 -0600

    built out settings more

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,8 +15,7 @@ const useStyles = createUseStyles({
             margin: 0,
         },
         body: {
-            height: '100vh',
-            backgroundColor: backgroundColor
+            height: '100vh'
         },
         '#root': {
             height: '100%'
@@ -24,13 +23,17 @@ const useStyles = createUseStyles({
     },
     root: {
         height: '100%',
+        width: '100%',
+        overflow: 'hidden'
+    },
+    wrapper: {
+        height: '100%',
         width: '800px',
         margin: '0 auto',
     },
     shade: {
         height: '100%',
         width: '100%',
-        background: `linear-gradient(0deg, rgba(0,0,0,0) 80%, ${backgroundColor} 100%)`,
         position: 'relative',
         zIndex: '100',
         pointerEvents: 'none'
@@ -57,41 +60,6 @@ const useStyles = createUseStyles({
     }
 });
 
-const terminalStyles = {
-    terminal: {
-        backgroundColor: 'none',
-        borderRadius: 0,
-        overflow: 'hidden',
-        maxHeight: '50%',
-        minHeight: '0px',
-        width: '700px',
-        position: 'absolute',
-        bottom: '50%',
-        fontSize: '20px'
-    },
-    content: {
-        color: '#fff',
-        fontSize: '20px',
-        lineHeight: '35px'
-    },
-    prompt: {
-        color: '#dcd05d',
-        fontSize: '20px',
-        lineHeight: '35px'
-    },
-    input: {
-        color: '#fff',
-        fontSize: '20px',
-        lineHeight: '40px',
-        height: '40px'
-    },
-    message: {
-        color: '#de8080',
-        fontSize: '20px',
-        lineHeight: '35px'
-    }
-}
-
 const defaultSearchEngines = {
     'google': {
         url: 'https://google.com'
@@ -105,7 +73,7 @@ const defaultSearchEngines = {
     'youtube': {
         url: 'https://www.youtube.com'
     }
-}
+};
 
 const commands = {
     echo: {
@@ -131,16 +99,83 @@ const commands = {
             return <span style={{color: '#f0f'}}>Text</span>
         }
     }
-}
+};
+
+const getOrSet = (key, value) => {
+    let prop = localStorage.getItem(key);
+    if (!prop) {
+        localStorage.setItem(key, value);
+        prop = value;
+    }
+    return prop;
+};
 
 export default () => {
     const classes = useStyles();
 
-    const [settings, setSettings] = useState(false);
+    const [settingsVisible, setSettingsVisible] = useState(false);
+    const [settings, setSettings] = useState({
+        backgroundColor: getOrSet('backgroundColor', backgroundColor),
+        promptColor: getOrSet('promptColor', '#dcd05d'),
+        contentColor: getOrSet('contentColor', '#ffffff'),
+        errorColor: getOrSet('errorColor', '#de8080')
+    });
+
+    const terminalStyles = {
+        terminal: {
+            backgroundColor: 'none',
+            borderRadius: 0,
+            overflow: 'hidden',
+            maxHeight: '50%',
+            minHeight: '0px',
+            width: '700px',
+            position: 'absolute',
+            bottom: '50%',
+            fontSize: '20px'
+        },
+        content: {
+            color: settings.contentColor,
+            fontSize: '20px',
+            lineHeight: '35px'
+        },
+        prompt: {
+            color: settings.promptColor,
+            fontSize: '20px',
+            lineHeight: '35px'
+        },
+        input: {
+            color: settings.contentColor,
+            fontSize: '20px',
+            lineHeight: '40px',
+            height: '40px'
+        },
+        message: {
+            color: settings.errorColor,
+            fontSize: '20px',
+            lineHeight: '35px'
+        }
+    }
 
     const settingsClick = () => {
-        setSettings(!settings);
+        setSettingsVisible(true);
     }
+
+    const onSettingsChange = (newSettings) => {
+        setSettings(newSettings);
+        console.log(settings);
+    }
+
+    const onSettingsClose = () => {
+        /*const keys = Object.keys(settings);
+        for (const key of keys) {
+            localStorage.setItem(key, settings[key]);
+        }*/
+        setSettingsVisible(false);
+    }
+
+    const setSettingValues = (event) => {
+
+    };
 
     const renderTerminal = () => {
         return (
@@ -155,22 +190,38 @@ export default () => {
                     inputTextStyle={terminalStyles.input}
                     messageStyle={terminalStyles.message}
                     styleEchoBack={'fullInherit'}
-                    welcomeMessage={'welcome\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'}
+                    welcomeMessage={'welcome'}
                     commands={commands}
                     autoFocus={true}
                     promptLabel={'>'}
                 />
-                <div className={classes.shade}/>
+                <div
+                    style={{
+                        background: `linear-gradient(0deg, rgba(0,0,0,0) 80%, ${settings.backgroundColor} 100%)`,
+                    }}
+                    className={classes.shade}
+                />
             </div>
         )
     }
 
     return (
-        <div className={classes.root}>
-            {settings
-                ? <h1>Settings</h1>
-                : renderTerminal()
-            }
+        <div
+            className={classes.root}
+            style={{
+                backgroundColor: settings.backgroundColor
+            }}
+        >
+            <div className={classes.wrapper}>
+                {settingsVisible
+                    ? <Settings
+                        settings={settings}
+                        onClose={onSettingsClose}
+                        onChange={onSettingsChange}
+                    />
+                    : renderTerminal()
+                }
+            </div>
         </div>
     )
 }
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -4,16 +4,89 @@ import Drawer from "@material-ui/core/Drawer";
 import SettingsIcon from '@material-ui/icons/Settings';
 import ClickAwayListener from '@material-ui/core/ClickAwayListener';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
 import {createUseStyles} from "react-jss";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {ChromePicker} from 'react-color'
 
 const useStyles = createUseStyles({
+    root: {
+        width: '500px',
+        margin: '0 auto',
+        backgroundColor: '#fff',
+        padding: '20px',
+        marginTop: '20%'
+    },
+    swatch: {
+        width: '100%',
+        height: '100%',
+        padding: '7px',
+        background: '#fff',
+        borderRadius: '1px',
+        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+        display: 'inline-block',
+        cursor: 'pointer',
+        boxSizing: 'border-box'
+    },
+    color: {
+        width: '100%',
+        height: '100%',
+        borderRadius: '2px',
+        boxSizing: 'border-box'
+    },
+    popover: {
+        position: 'absolute',
+        zIndex: '2',
+    },
+    cover: {
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        bottom: '0px',
+        left: '0px',
+    },
+    header: {
+        paddingBottom: '30px'
+    }
+});
 
-})
+const ColorPicker = (props) => {
+    const classes = useStyles();
+
+    const [showPicker, setShowPicker] = useState(false);
+    const [color, setColor] = useState(props.color);
+
+    const colorChangeHandler = (color) => {
+        setColor(color.hex);
+        props.onChange(color);
+    }
+
+    return (
+        <>
+            <div className={classes.swatch} onClick={() => setShowPicker(!showPicker)}>
+                <div
+                    style={{
+                        backgroundColor: color
+                    }}
+                    className={classes.color}
+                />
+            </div>
+            {showPicker ? <div className={classes.popover}>
+                <div className={classes.cover} onClick={() => setShowPicker(!showPicker)}/>
+                <ChromePicker color={color} onChange={colorChangeHandler}/>
+            </div> : null}
+        </>
+    )
+};
 
 export default (props) => {
     const classes = useStyles();
     const [open, setOpen] = useState(false);
+    const [settings, setSettings] = useState(props.settings);
+    const [showBGColor, setShowBGColor] = useState(false);
 
     const settingsClick = () => {
         setOpen(!open);
@@ -25,8 +98,89 @@ export default (props) => {
         }
     }
 
+    const bgColorHandler = (color) => {
+        setSettings({
+            ...settings,
+            backgroundColor: color.hex
+        })
+        localStorage.setItem('backgroundColor', color.hex);
+        props.onChange(settings);
+    }
+
+    const promptColorHandler = (color) => {
+        setSettings({
+            ...settings,
+            promptColor: color.hex
+        })
+        localStorage.setItem('promptColor', color.hex);
+        props.onChange(settings);
+    }
+
+    const contentColorHandler = (color) => {
+        setSettings({
+            ...settings,
+            contentColor: color.hex
+        })
+        localStorage.setItem('contentColor', color.hex);
+        props.onChange(settings);
+    }
+
+    const errorColorHandler = (color) => {
+        setSettings({
+            ...settings,
+            errorColor: color.hex
+        })
+        localStorage.setItem('errorColor', color.hex);
+        props.onChange(settings);
+    }
+
+    useEffect(() => {
+
+    }, [settings]);
+
     return (
-        <div>
+        <div className={classes.root}>
+            <Box className={classes.header}>
+                <Button variant="contained" color="primary" onClick={props.onClose}>Close</Button>
+            </Box>
+            <Grid container spacing={3}>
+                <Grid item xs={4}>
+                    <ColorPicker
+                        onChange={bgColorHandler}
+                        color={settings.backgroundColor}
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <Typography variant="h5">Background Color</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <ColorPicker
+                        onChange={promptColorHandler}
+                        color={settings.promptColor}
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <Typography variant="h5">Prompt Text Color</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <ColorPicker
+                        onChange={contentColorHandler}
+                        color={settings.contentColor}
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <Typography variant="h5">Content Text Color</Typography>
+                </Grid>
+                <Grid item xs={4}>
+                    <ColorPicker
+                        onChange={errorColorHandler}
+                        color={settings.errorColor}
+                    />
+                </Grid>
+                <Grid item xs={8}>
+                    <Typography variant="h5">Error Text Color</Typography>
+                </Grid>
+            </Grid>
 
         </div>
     );
